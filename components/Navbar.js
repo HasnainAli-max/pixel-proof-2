31963fe9bf9ee07b4886e2a8a72485380c6157e0
HasnainAli <ha3710191@gmail.com>
2025-08-25 20:13:15 +0500

    add link on navbar logo image

--- components/Navbar.js
+++ components/Navbar.js
@@ -138,7 +138,9 @@ export default function Navbar({ user: userProp, onSignOut }) {
       <div className="max-w-7xl mx-auto px-4 py-3 flex items-center justify-between">
         {/* Brand */}
         <div className="flex items-center space-x-2">
+          <Link href="/utility" >
           <img src="/logo.svg" alt="Logo" className="h-6 w-6" />
+          </Link>
           <span className="text-lg font-bold text-purple-800 dark:text-purple-300">PixelProof</span>
         </div>
 
